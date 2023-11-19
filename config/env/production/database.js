46eefb109c451456f1d0e2a264ee2a0f3ee9d9d7
Morpheous <elihu@marketinc.mx>
2023-11-18 20:21:39 -0600

    Add prod config & nixpacks

---
+++ config/env/production/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true),
+      },
+    },
+    debug: true,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+});
